3dedd3063f99f7a6153d808922c32e3565355fba
Raphael van Marion <raphaeldarealmvp@gmail.com>
2023-06-06 11:25:18 +0200

    Added order service config

--- src/main/java/com/drossdrop/apigateway/filter/RouteValidator.java
+++ src/main/java/com/drossdrop/apigateway/filter/RouteValidator.java
@@ -12,7 +12,8 @@ public class RouteValidator {
     public static final List<String> openApiEndpoints = List.of(
         "/api/auth/register",
         "/api/auth/token",
-        "/eureka"
+        "/eureka",
+        "/api/order/**"
     );
 
     public Predicate<ServerHttpRequest> isSecured = request -> openApiEndpoints.stream()
